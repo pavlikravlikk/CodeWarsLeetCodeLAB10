049dd6897210bb69ea6d34d5b8bc8c581bba9b64
pavlikravlikk <krailo.pn@ucu.edu.ua>
2026-04-21 19:58:32 +0300

    Update deletenodeinabst.py

--- DeleteNodeInaBST/deletenodeinabst.py
+++ DeleteNodeInaBST/deletenodeinabst.py
@@ -1,1 +1,23 @@
-
+class Solution:
+    def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
+        if not root:
+            return None
+        
+        if key < root.val:
+            root.left = self.deleteNode(root.left, key)
+        elif key > root.val:
+            root.right = self.deleteNode(root.right, key)
+        else:
+            if not root.left:
+                return root.right
+            elif not root.right:
+                return root.left
+            
+            min_node = root.right
+            while min_node.left:
+                min_node = min_node.left
+            
+            root.val = min_node.val
+            root.right = self.deleteNode(root.right, root.val)
+            
+        return root
